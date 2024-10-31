72826f357da2184ae4e25c0607bb77081a3b7958
AnataAria <kakata28419@gmail.com>
2024-11-01 06:53:43 +0700

    Fetch UI Main

--- app/src/main/java/com/theanimegroup/movie_ticket_booking_client/ui/adapters/MovieAdapter.java
+++ app/src/main/java/com/theanimegroup/movie_ticket_booking_client/ui/adapters/MovieAdapter.java
@@ -5,7 +5,7 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,34 +13,17 @@ import android.widget.TextView;
 import com.theanimegroup.movie_ticket_booking_client.R;
 import com.theanimegroup.movie_ticket_booking_client.models.entity.Movie;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class MovieAdapter extends BaseAdapter {
-    private Context context;
-    private ArrayList<Movie> movies;
-
-    public MovieAdapter(Context context, ArrayList<Movie> movies) {
-        this.context = context;
-        this.movies = movies;
+public class MovieAdapter extends BaseListAdapter<Movie> {
+    public MovieAdapter(Context movieActivity, List<Movie> movieList) {
+        super(movieActivity, movieList);
     }
 
-    @Override
-    public int getCount() {
-        return movies.size();
-    }
-    @Override
-    public Object getItem(int position) {
-        return movies.get(position);
-    }
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.activity_movie, parent, false);
+            convertView = inflater.inflate(R.layout.activity_movie, parent, false);
         }
 
         TextView id = convertView.findViewById(R.id.movie_id);
@@ -54,9 +37,7 @@ public class MovieAdapter extends BaseAdapter {
         TextView dn = convertView.findViewById(R.id.movie_director_name);
         TextView ls = convertView.findViewById(R.id.movie_list);
 
-
-
-        Movie movie = movies.get(position);
+        Movie movie = items.get(position);
         id.setText(String.valueOf(movie.getId()));
         name.setText(movie.getName());
         description.setText(movie.getDescription());
